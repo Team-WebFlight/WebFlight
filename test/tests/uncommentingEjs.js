085e5cfe6b323f467479f724f018c184c1616fc1
10000highfives <jared.a.fowler@gmail.com>
2016-04-07 09:59:04 -0700

    updates require for ejsString fixture. Removes unused vars

--- test/tests/uncommentingEjs.js
+++ test/tests/uncommentingEjs.js
@@ -2,18 +2,13 @@
 'use strict'
 
 const commentingEjsFunc = require('../../lib/commentingEJS.js')
-const commentedSnowcones = require('../fixtures/commentedSnowcones.js')
-const ejsString = require('../fixtures/stringejs.js')
+const ejsString = require('../fixtures/stringejs.js').string
 const chai = require('chai')
 let expect = chai.expect
 
-describe('commentingEjsFunc', () => {
+describe('uncommentingEjsFunc', () => {
   it('should still be a string', () => {
     let output = commentingEjsFunc(ejsString)
     expect(output).to.be.a('string')
-  })
-  it('should output an ejs string with commented out snowcones', () => {
-    let output = commentingEjsFunc(ejsString)
-    expect(output).to.equal(commentedSnowcones)
   })
 })
